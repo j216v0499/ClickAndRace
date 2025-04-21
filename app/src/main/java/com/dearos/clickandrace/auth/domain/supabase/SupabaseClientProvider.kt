ec69779f1ac88991e768e35cc2c86e9e144625a6
J. Antonio D. P. <juanantoniodearos@gmail.com>
2025-04-21 12:13:47 +0200

    SupabaseClientProvider

---
+++ app/src/main/java/com/dearos/clickandrace/auth/domain/supabase/SupabaseClientProvider.kt
@@ -0,0 +1,27 @@
+package com.dearos.clickandrace.auth.domain.supabase
+
+import com.dearos.clickandrace.BuildConfig
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.postgrest.Postgrest
+
+object SupabaseClientProvider {
+
+                                                //build.gradle.kts :app
+    private const val SUPABASE_URL =            BuildConfig.SUPABASE_URL
+    private const val SUPABASE_ANON_KEY =       BuildConfig.SUPABASE_ANON_KEY
+
+    val client: SupabaseClient by lazy {
+        createSupabaseClient(
+            supabaseUrl = SUPABASE_URL,
+            supabaseKey = SUPABASE_ANON_KEY
+        ) {
+            install(Auth) {
+                alwaysAutoRefresh = true
+                autoLoadFromStorage = true
+            }
+            install(Postgrest)
+        }
+    }
+}
