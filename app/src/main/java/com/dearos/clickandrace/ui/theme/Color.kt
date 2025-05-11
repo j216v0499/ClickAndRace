17c6adfb31a328d053cf6700133470ee121f39dd
J. Antonio D. P. <juanantoniodearos@gmail.com>
2025-05-11 11:21:51 +0200

    Ajustes de tema de la aplicación

--- app/src/main/java/com/dearos/clickandrace/ui/theme/Color.kt
+++ app/src/main/java/com/dearos/clickandrace/ui/theme/Color.kt
@@ -9,3 +9,13 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+//Colores partida logi
+//claro
+val ColorWhite100 = Color(0xFFFAFAFA)
+val ColorDark200 = Color(0xFF000000)
+
+//oscuro
+val ColorDark100 = Color(0xFF000000)
+val ColorWhite200 = Color(0xFFFAFAFA)
--- app/src/main/java/com/dearos/clickandrace/ui/theme/Theme.kt
+++ app/src/main/java/com/dearos/clickandrace/ui/theme/Theme.kt
@@ -8,18 +8,30 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
     secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+    tertiary = Pink80,
+    background = ColorDark100,
+    onPrimary = ColorWhite200
+
+    )
 
 private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
-    tertiary = Pink40
+    tertiary = Pink40,
+    //Color fondo
+    background = ColorWhite100,
+    //Color texto negro
+    onPrimary = ColorDark200,
+
+    //Color
+
+
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
