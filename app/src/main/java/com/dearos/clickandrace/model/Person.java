8f47136a5e2e1e5ace7d8224c033b1b66aff3225
J. Antonio D. P. <juanantoniodearos@gmail.com>
2025-05-11 11:18:32 +0200

    Cambios no significativos

--- app/src/main/java/com/dearos/clickandrace/model/Person.java
+++
@@ -1,9 +0,0 @@
-package com.dearos.clickandrace.model;
-
-public class Person{
-    private String name;
-    private String userImg;
-    private String descrip;
-    private Double rating; //del 1 al 5 en estrellas
-
-}
